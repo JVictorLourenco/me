d79a80be9e00d942423b94c971de0cca406da4c1
João Victor <54009514+T4SUYA@users.noreply.github.com>
2023-05-23 11:37:12 -0300

    Corrected personal info and start about page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Particles from "./components/particles";
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
+  { name: "About Me", href: "/about" },
 ];
 
 export default function Home() {
@@ -28,28 +29,18 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-8xl whitespace-nowrap bg-clip-text ">
         João Lourenco
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 max-w-5xl">
+      <div className="my-12 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 max-w-3xl px-5">
           I'm João Victor R. Lourenco, a Software Engineer with 4+ years of
-          full-stack experience. Currently, I work at{" "}
-          <Link
-            target="_blank"
-            href="https://upstash.com"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            Cleverti
-          </Link>{" "}
-          where I contribute my expertise in developing robust solutions across
-          the stack. With a strong engineering background and proficiency in
-          multiple programming languages, I thrive in tackling complex
-          challenges. Continuous learning is a priority for me to stay updated
-          with industry trends and deliver high-quality results. Let's push the
-          boundaries of technology together.
+          full-stack experience. I excel in developing robust solutions across
+          the stack, with a strong engineering background and proficiency in
+          multiple programming languages. Let's push the boundaries of
+          technology together.
           <br />
         </h2>
       </div>
